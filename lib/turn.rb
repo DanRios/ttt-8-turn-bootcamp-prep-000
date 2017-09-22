7a62d3705d723ffa940ce5c25e7d91d53db2015b
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-22 00:14:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,8 +33,6 @@ else index > 9 || position_taken?(board,index)
 end
 end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
