4d410e9c25e5a299bd7ac016f9bd230969a7d520
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-22 00:34:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,5 +32,14 @@ end
 
 def turn(board)
   puts "Please enter 1-9"
+  input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+    move(board,index)
+    display_board(board)
+  else valid_move?(board, index == false)
+    puts "That move is not valid. Please try again."
+    input = gets.strip
+end
 end
 
