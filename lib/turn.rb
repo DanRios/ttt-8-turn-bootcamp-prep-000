577fe9c367d034b6309f3496084516582f670a8f
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-22 00:11:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,12 @@
+board = [" ", " ", " "," ", " ", " ", " ", " ", " "]
 
+def display_board(move = board)
+  puts " #{move[0]} | #{move[1]} | #{move[2]} "
+  puts "-----------"
+  puts " #{move[3]} | #{move[4]} | #{move[5]} "
+  puts "-----------"
+  puts " #{move[6]} | #{move[7]} | #{move[8]} "
+end
 
 
 
